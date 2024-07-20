b72250e261180a1ccb7b56c53276421fb864ea99
Umang Pincha <umangpincha@gmail.com>
2024-07-20 13:49:57 +0530

    Delete Class 01 (Inception) 24th.Dec.22 directory

--- Class 01 (Inception) 24th.Dec.22/Basic Hello World Using JS/app.js
+++
@@ -1,6 +0,0 @@
-const heading = document.createElement("h1");
-heading.innerText = "Hello World!";
-// console.log(heading);
-const root = document.getElementById("root");
-// console.log(root);
-root.appendChild(heading);
