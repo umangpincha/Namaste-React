3aedd569c0fb982be46aef2855c4c3eb02443431
umangpincha <umangpincha@gmail.com>
2026-09-03 23:52:15 +0530

    Add Ep-01

---
+++ Ep-01-Inception/script.js
@@ -0,0 +1,25 @@
+// let heading = React.createElement("h1" , {id:"heading"} , "Hello from React!");
+
+// console.log(heading);
+// let root = ReactDOM.createRoot(document.querySelector("#root"));
+// // root.render(heading)
+
+// // TODO: Yahan upar {id:"heading"} , "Hello from React!" props hain 
+
+// const parent = React.createElement("div" , {id:"parent"} , [
+//     React.createElement("div", {id:"child"} , [
+//         React.createElement("h1" , {} , "I am h1 tag"),
+//         React.createElement("h1" , {} , "I am h1 tag")
+//     ]) , 
+//     React.createElement("div", {id:"child2"} , [
+//         React.createElement("h1" , {} , "I am h1 tag"),
+//         React.createElement("h1" , {} , "I am h1 tag")
+//     ]) , 
+// ])
+
+// root.render(parent)
+
+// // Es upar wale tareeke se bhaut mushkil hoti hai html banana , toh esliye hum yeh React.createElement use kr rhe hain , thats why JSX introduce hua
+
+
+throw new Error("Something went wrong!");
